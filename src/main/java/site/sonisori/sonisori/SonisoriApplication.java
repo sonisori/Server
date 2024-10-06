3faf0d19160196a8f3dc8742cc64767430b7a385
Hanbi <adray0625@naver.com>
2024-10-06 20:18:04 +0900

    initial commit

---
+++ src/main/java/site/sonisori/sonisori/SonisoriApplication.java
@@ -0,0 +1,13 @@
+package site.sonisori.sonisori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonisoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonisoriApplication.class, args);
+	}
+
+}
