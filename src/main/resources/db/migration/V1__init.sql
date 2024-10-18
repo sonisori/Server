1bd13314398d96ca5fbc2466d8b027683a524c7e
Hanbi <adray0625@naver.com>
2024-10-18 22:27:01 +0900

    chore: Docker Compose를 이용한 MySQL, Redis 및 Flyway 마이그레이션 구현 (#5)
    * chore: mysql, flyway, redis 관련 초기 설정 및 docker-compose 파일 작성
    
    * refactor: 경량화된 redis로 변경
    
    * chore: mysql flyway schema 작성
    
    * chore: db sql 수정
    
    * chore: sql 수정

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,72 @@
+CREATE TABLE IF NOT EXISTS `sonisori`.`users` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `email` VARCHAR(45) NOT NULL,
+    `role` ENUM('ROLE_USER','ROLE_ADMIN') NOT NULL DEFAULT 'ROLE_USER',
+    `username` VARCHAR(500) NOT NULL,
+    `social_type` ENUM('kakao','google') NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `sonisori`.`sign_topics` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(45) NOT NULL,
+    `contents` VARCHAR(255) NOT NULL,
+    `difficulty` ENUM('EASY','MEDIUM','HARD') NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `sonisori`.`sign_quizzes` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `sign_topic_id` BIGINT NOT NULL,
+    `sentence` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `sonisori`.`quiz_histories` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT NOT NULL,
+    `sign_topic_id` BIGINT NOT NULL,
+    `count` INT NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `sonisori`.`sign_words` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `word` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+ALTER TABLE `sonisori`.`sign_quizzes`
+    ADD INDEX `fk_sign-topics_sign-quizzes_id_idx` (`sign_topic_id` ASC) VISIBLE;
+
+ALTER TABLE `sonisori`.`sign_quizzes`
+    ADD CONSTRAINT `fk_sign-topics_sign-quizzes_id`
+        FOREIGN KEY (`sign_topic_id`)
+            REFERENCES `sonisori`.`sign_topics` (`id`)
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION;
+
+ALTER TABLE `sonisori`.`quiz_histories`
+    ADD INDEX `fk_quiz-histories_sign-topics_id_idx` (`sign_topic_id` ASC) VISIBLE,
+    ADD INDEX `fk_quiz-histories_users_id_idx` (`user_id` ASC) VISIBLE;
+
+ALTER TABLE `sonisori`.`quiz_histories`
+    ADD CONSTRAINT `fk_quiz-histories_sign-topics_id`
+        FOREIGN KEY (`sign_topic_id`)
+            REFERENCES `sonisori`.`sign_topics` (`id`)
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION,
+    ADD CONSTRAINT `fk_quiz-histories_users_id`
+        FOREIGN KEY (`user_id`)
+            REFERENCES `sonisori`.`users` (`id`)
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION;
