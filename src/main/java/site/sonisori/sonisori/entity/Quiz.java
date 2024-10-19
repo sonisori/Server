f679b4cdc3905903704cef9ae378b12f260fddf6
이규빈 <kevin032700@naver.com>
2024-10-19 21:15:56 +0900

    feat : Quiz 엔티티 구현

---
+++ src/main/java/site/sonisori/sonisori/entity/Quiz.java
@@ -0,0 +1,40 @@
+package site.sonisori.sonisori.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "sign_quizzes")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quiz extends Data {
+
+	@Id
+	@NotNull
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@NotNull
+	@JoinColumn(name = "sign_topic_id")
+	private Topic topic;
+
+	@NotNull
+	@Column(name = "senetece", length = 255)
+	private String sentece;
+
+}
