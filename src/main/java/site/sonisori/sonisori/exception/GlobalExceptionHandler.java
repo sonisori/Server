91d50848542a71824f71fba99ddc3620c2b01642
Hanbi <adray0625@naver.com>
2024-10-27 11:25:54 +0900

    feat: slf4j 로그 설정 추가 및 적용 (#17)

--- src/main/java/site/sonisori/sonisori/exception/GlobalExceptionHandler.java
+++ src/main/java/site/sonisori/sonisori/exception/GlobalExceptionHandler.java
@@ -11,15 +11,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
+import lombok.extern.slf4j.Slf4j;
 import site.sonisori.sonisori.common.constants.ErrorMessage;
 import site.sonisori.sonisori.common.response.ErrorResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	private static final String DEFAULT_MESSAGE = ErrorMessage.INVALID_REQUEST.getMessage();
 
 	@ExceptionHandler(HandlerMethodValidationException.class)
 	public ResponseEntity<ErrorResponse> handlerMethodValidationException(HandlerMethodValidationException ex) {
+		logException(ex);
 		String errorMessage = ex.getAllValidationResults()
 			.getFirst()
 			.getResolvableErrors()
@@ -32,6 +35,7 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+		logException(ex);
 		List<FieldError> fieldErrors = ex.getFieldErrors();
 		String errorMessage = fieldErrors.stream()
 			.map(FieldError::getDefaultMessage)
@@ -45,19 +49,26 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException ex) {
+		logException(ex);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 			.body(new ErrorResponse(ErrorMessage.NULL_POINTER_EXCEPTION.getMessage()));
 	}
 
 	@ExceptionHandler(RuntimeException.class)
 	public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
+		logException(ex);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 			.body(new ErrorResponse(ErrorMessage.SERVER_ERROR.getMessage()));
 	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+		logException(ex);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 			.body(new ErrorResponse(ErrorMessage.SERVER_ERROR.getMessage()));
+	}
+
+	private void logException(Exception ex) {
+		log.error("Unhandled exception: [{}] - {}", ex.getClass().getSimpleName(), ex.getMessage());
 	}
 }
