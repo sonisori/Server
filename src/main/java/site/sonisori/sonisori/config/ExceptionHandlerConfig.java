8c84f191ea7bc8789e745383a7549f2217ee532a
Hanbi <adray0625@naver.com>
2024-10-30 11:55:31 +0900

    feat: security config 설정 추가

---
+++ src/main/java/site/sonisori/sonisori/config/ExceptionHandlerConfig.java
@@ -0,0 +1,54 @@
+package site.sonisori.sonisori.config;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import site.sonisori.sonisori.common.constants.ErrorMessage;
+import site.sonisori.sonisori.common.response.ErrorResponse;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ExceptionHandlerConfig implements Customizer<ExceptionHandlingConfigurer<HttpSecurity>> {
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public void customize(ExceptionHandlingConfigurer<HttpSecurity> httpSecurityExceptionHandlingConfigurer) {
+		httpSecurityExceptionHandlingConfigurer
+			.authenticationEntryPoint(this::handleAuthenticationException)
+			.accessDeniedHandler(this::handleAccessDeniedException);
+	}
+
+	private void handleAuthenticationException(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException) throws IOException {
+		log.error("Unauthorized request - Method: {}, URI: {}, Error: {}",
+			request.getMethod(),
+			request.getRequestURI(),
+			authException.getMessage());
+
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		response.setContentType("application/json");
+
+		ErrorResponse errorResponse = new ErrorResponse(ErrorMessage.NOT_FOUND_TOKEN.getMessage());
+
+		String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+		response.getWriter().write(jsonResponse);
+	}
+
+	private void handleAccessDeniedException(HttpServletRequest request, HttpServletResponse response,
+		AccessDeniedException accessDeniedException) throws IOException {
+		response.sendError(HttpServletResponse.SC_FORBIDDEN);
+	}
+}
