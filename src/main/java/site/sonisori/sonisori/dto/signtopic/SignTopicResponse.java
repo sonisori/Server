0f562512d44045ce191f95945e43c3bd1bceacc2
Hanbi <adray0625@naver.com>
2024-11-14 15:46:43 +0900

    feat: dto 작성

---
+++ src/main/java/site/sonisori/sonisori/dto/signtopic/SignTopicResponse.java
@@ -0,0 +1,13 @@
+package site.sonisori.sonisori.dto.signtopic;
+
+import lombok.Builder;
+import site.sonisori.sonisori.common.enums.Difficulty;
+
+@Builder
+public record SignTopicResponse(
+	Long id,
+	String title,
+	String contents,
+	Difficulty difficulty
+) {
+}
