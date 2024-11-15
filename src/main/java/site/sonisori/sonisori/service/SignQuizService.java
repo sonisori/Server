3515ae543d99292a1bcc40d7f23542d33087b608
Hanbi <adray0625@naver.com>
2024-11-15 22:56:39 +0900

    refactor: 불필요 변수 삭제

--- src/main/java/site/sonisori/sonisori/service/SignQuizService.java
+++ src/main/java/site/sonisori/sonisori/service/SignQuizService.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import site.sonisori.sonisori.common.constants.ErrorMessage;
 import site.sonisori.sonisori.dto.signquiz.SignQuizResponse;
 import site.sonisori.sonisori.entity.SignQuiz;
-import site.sonisori.sonisori.entity.SignTopic;
 import site.sonisori.sonisori.exception.NotFoundException;
 import site.sonisori.sonisori.repository.SignQuizRepository;
 import site.sonisori.sonisori.repository.SignTopicRepository;
@@ -23,8 +22,9 @@ public class SignQuizService {
 	public List<SignQuizResponse> findQuizzesByTopicId(
 		Long signTopicId
 	) {
-		SignTopic signTopic = signTopicRepository.findById(signTopicId)
+		signTopicRepository.findById(signTopicId)
 			.orElseThrow(()-> new NotFoundException(ErrorMessage.NOT_FOUND_TOPIC.getMessage()));
+
 		List<SignQuiz> signQuizzes = signQuizRepository.findAllBySignTopic_id(signTopicId);
 
 		return signQuizzes.stream()
