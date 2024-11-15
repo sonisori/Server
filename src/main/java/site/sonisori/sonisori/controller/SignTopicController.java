773f5177d4f8418449af8edff77006b6c6d29204
Hanbi <adray0625@naver.com>
2024-11-15 11:22:00 +0900

    feat: controller 로직 작성

---
+++ src/main/java/site/sonisori/sonisori/controller/SignTopicController.java
@@ -0,0 +1,31 @@
+package site.sonisori.sonisori.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.sonisori.sonisori.auth.CustomUserDetails;
+import site.sonisori.sonisori.dto.signtopic.SignTopicResponse;
+import site.sonisori.sonisori.service.SignTopicService;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class SignTopicController {
+
+	private final SignTopicService signTopicService;
+
+	@GetMapping("/topics")
+	public ResponseEntity<List<SignTopicResponse>> getTopicsWithUserStatus(
+		@AuthenticationPrincipal CustomUserDetails userDetails
+	) {
+		Long userId = userDetails.getUserId();
+		List<SignTopicResponse> topics = signTopicService.getTopicsWithStatus(userId);
+		return ResponseEntity.ok(topics);
+	}
+}
