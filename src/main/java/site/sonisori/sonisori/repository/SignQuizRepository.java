15145fd0c65b8289718250628e329b8f374fd741
Hanbi <adray0625@naver.com>
2024-11-15 19:18:30 +0900

    refactor: 로직 수정

--- src/main/java/site/sonisori/sonisori/repository/SignQuizRepository.java
+++
@@ -1,9 +0,0 @@
-package site.sonisori.sonisori.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import site.sonisori.sonisori.entity.SignQuiz;
-
-public interface SignQuizRepository extends JpaRepository<SignQuiz, Long> {
-	int countBySignTopic_id(Long signTopicId);
-}
