423b242637f3e8c02814e0012ed28c25a7770aa0
Hanbi <adray0625@naver.com>
2024-11-22 20:44:29 +0900

    feat: service 로직 작성

---
+++ src/main/java/site/sonisori/sonisori/service/QuizHistoryService.java
@@ -0,0 +1,45 @@
+package site.sonisori.sonisori.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import site.sonisori.sonisori.common.constants.ErrorMessage;
+import site.sonisori.sonisori.dto.quizhistory.QuizHistoryRequest;
+import site.sonisori.sonisori.dto.quizhistory.QuizHistoryResponse;
+import site.sonisori.sonisori.entity.QuizHistory;
+import site.sonisori.sonisori.entity.SignTopic;
+import site.sonisori.sonisori.entity.User;
+import site.sonisori.sonisori.exception.NotFoundException;
+import site.sonisori.sonisori.repository.QuizHistoryRepository;
+import site.sonisori.sonisori.repository.SignTopicRepository;
+import site.sonisori.sonisori.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class QuizHistoryService {
+	private final QuizHistoryRepository quizHistoryRepository;
+	private final SignTopicRepository signTopicRepository;
+	private final UserRepository userRepository;
+
+	public QuizHistoryResponse submitQuizResult(
+		Long userId, Long topicId, QuizHistoryRequest quizHistoryRequest
+	) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_USER.getMessage()));
+		SignTopic signTopic = signTopicRepository.findById(topicId)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.NOT_FOUND_TOPIC.getMessage()));
+
+		QuizHistory quizHistory = QuizHistory.builder()
+			.user(user)
+			.signTopic(signTopic)
+			.correctCount(quizHistoryRequest.correctCount())
+			.build();
+		quizHistoryRepository.save(quizHistory);
+
+		return new QuizHistoryResponse(
+			signTopic.getTitle(),
+			quizHistoryRequest.correctCount(),
+			signTopic.getTotalQuizzes()
+		);
+	}
+}
