b9738f61929599943835c76d092e72747002260f
Hanbi <adray0625@naver.com>
2024-12-05 10:31:46 +0900

    chore: sign word db 수정

---
+++ src/main/resources/db/migration/V4__modify_sign_word_table.sql
@@ -0,0 +1,24 @@
+ALTER TABLE `sonisori`.`sign_words`
+    ADD COLUMN `description` VARCHAR(500) NULL AFTER `word`,
+    ADD COLUMN `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() AFTER `description`,
+    ADD COLUMN `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP() AFTER `created_at`;
+
+CREATE TABLE `sonisori`.`sign_word_resources` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `sign_word_id` BIGINT NOT NULL,
+  `type` ENUM('image', 'video') NOT NULL,
+  `resource_url` VARCHAR(500) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+ALTER TABLE `sonisori`.`sign_word_resources`
+    ADD INDEX `fk_sign-word-resources_sign-word_id_idx` (`sign_word_id` ASC) VISIBLE;
+
+ALTER TABLE `sonisori`.`sign_word_resources`
+    ADD CONSTRAINT `fk_sign-word-resources_sign-word_id`
+        FOREIGN KEY (`sign_word_id`)
+            REFERENCES `sonisori`.`sign_words` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION;
