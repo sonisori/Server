b3954176d3962a21f16887a53eec50c4ec216212
Hanbi <adray0625@naver.com>
2024-12-05 13:31:34 +0900

    feat: controller 로직 작성

---
+++ src/main/java/site/sonisori/sonisori/controller/SignWordController.java
@@ -0,0 +1,25 @@
+package site.sonisori.sonisori.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.sonisori.sonisori.dto.signword.SignWordResponse;
+import site.sonisori.sonisori.service.SignWordService;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class SignWordController {
+	private final SignWordService signWordService;
+
+	@GetMapping("/words")
+	public ResponseEntity<List<SignWordResponse>> getAllSignWords() {
+		List<SignWordResponse> signWords = signWordService.fetchSignWords();
+		return ResponseEntity.ok(signWords);
+	}
+}
