da366ac5b8b09c27beb433e2d5078669bd71e360
이규빈 <kevin032700@naver.com>
2024-12-08 22:55:31 +0900

    feat: 관리자 단어 수정기능 구현

--- src/main/java/site/sonisori/sonisori/controller/SignWordController.java
+++ src/main/java/site/sonisori/sonisori/controller/SignWordController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,6 +43,14 @@ public class SignWordController {
 		@PathVariable(name = "wordId") Long wordId
 	) {
 		signWordService.deleteSignWord(wordId);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
+	@PatchMapping("/admin/words/{wordId}")
+	public ResponseEntity<Void> updateSignWordByAdmin(@Valid @RequestBody SignWordRequest signWordRequest,
+		@PathVariable(name = "wordId") Long wordId
+	) {
+		signWordService.updateSignWord(wordId, signWordRequest);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
